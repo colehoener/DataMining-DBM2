e2ce7ecc938cf21825f20e10806e8b7c9ab80d0d
colehoener <colehoener@gmail.com>
2021-12-08 17:38:39 +0100

    minor changes

--- fixDatesColumn.py
+++
@@ -1,9 +0,0 @@
-import csv
-import re
-
-with open('cleaned_data.csv', 'r') as fin, open("out.csv",'w') as fout:
-        writer = csv.writer(fout)
-        for row in csv.reader(fin):
-            row[1] = re.sub(r'-1,', '-10,',row[1])
-            writer.writerow(row)
-
